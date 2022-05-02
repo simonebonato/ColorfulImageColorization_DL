c2f25087082f327664137b664fc099dabb253922
simon <bonato@kth.se>
2022-05-02 16:13:37 +0200

    ImageDataGenerator bullcrap

--- temp.py
+++ temp.py
@@ -2,25 +2,37 @@ import cv2
 import numpy as np
 from PIL import Image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+from skimage import io, color
+
 
 def myFunc(image):
-    image = np.array(image)
-    hsv_image = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
-    return Image.fromarray(hsv_image)
+    lab = color.rgb2lab(np.float32(image) / 255.)
+    # lab = color.rgb2lab(image)
+    return lab
+
+
+def plot_image(img):
+    img = color.lab2rgb(img)
+    plt.imshow(img)
+    # plt.set_cmap('gray')
+    plt.show()
 
 
 if __name__ == '__main__':
+    # datagen = ImageDataGenerator(preprocessing_function=myFunc)
+    # path = r'C:\Users\simon\PycharmProjects\DeepLearningPython\GroupProject\test'
+    # generator = datagen.flow_from_directory(directory=path, target_size=(224, 224), class_mode=None, batch_size=1)
 
-    datagen = ImageDataGenerator(
-        featurewise_center=True,
-        featurewise_std_normalization=True,
-        rotation_range=20,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        horizontal_flip=True,
-        validation_split=0.2,
-        preprocessing_function=myFunc
-    )
+    datagen = ImageDataGenerator(preprocessing_function=myFunc)
     path = r'C:\Users\simon\PycharmProjects\DeepLearningPython\GroupProject\test'
-    datagen.flow_from_directory(directory=path, target_size=(224, 224), class_mode=None)
+    generator = datagen.flow_from_directory(directory=path,
+                                            target_size=(224, 224),
+                                            class_mode=None,
+                                            batch_size=1,
+                                            shuffle=False)
 
+    for i in generator:
+        plot_image(i[0])
+        break
