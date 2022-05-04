5d8c52fea52d04ed68e14e019db3a803df1eeaf9
simon <bonato@kth.se>
2022-05-04 12:12:34 +0200

    (probably) final version of the network

--- CNN_class.py
+++ CNN_class.py
@@ -18,72 +18,75 @@ class CNN:
         # self.load_images_from_folder('data')
         self.model = Sequential()
         self.model.add(Input(
-            shape=(224, 224, 1),
-            # batch_size=None,
+            shape=(256, 256, 1),
+            batch_size = 1,
             name='input',
         ))
-        # layer structure (X, C, S, D, BN, P)
+
         # see Table 4 - page 24
-        # X : output size after applying this operation
         # C : number of filters
         # S : stride
         # D : kernel dilation
         # BN : Batch norm (True or False)
         # Padding : True - same, False - valid
+        # layer structure ('name', C, S, D, K, BN)
         conv_layers = (
-            ('conv1_1', 224, 64, 1, 1, False, True),
-            ('conv1_2', 112, 64, 2, 1, True, False),
+            ('conv1_1', 64, 1, 1, 3, False),
+            ('conv1_2', 64, 2, 1, 3, True),
 
-            ('conv2_1', 112, 128, 1, 1, False, True),
-            ('conv2_2', 56, 128, 2, 1, True, False),
+            ('conv2_1', 128, 1, 1, 3, False),
+            ('conv2_2', 128, 2, 1, 3, True),
 
-            ('conv3_1', 56, 256, 1, 1, False, True),
-            ('conv3_2', 56, 256, 1, 1, False, True),
-            ('conv3_3', 28, 256, 2, 1, True, False),
+            ('conv3_1', 256, 1, 1, 3, False),
+            ('conv3_2', 256, 1, 1, 3, False),
+            ('conv3_3', 256, 2, 1, 3, True),
 
-            ('conv4_1', 28, 512, 1, 1, False, True),
-            ('conv4_2', 28, 512, 1, 1, False, True),
-            ('conv4_3', 28, 512, 1, 1, True, True),
+            ('conv4_1', 512, 1, 1, 3, False),
+            ('conv4_2', 512, 1, 1, 3, False),
+            ('conv4_3', 512, 1, 1, 3, True),
 
-            ('conv5_1', 28, 512, 1, 2, False, True),
-            ('conv5_2', 28, 512, 1, 2, False, True),
-            ('conv5_3', 28, 512, 1, 2, True, True),
+            ('conv5_1', 512, 1, 2, 3, False),
+            ('conv5_2', 512, 1, 2, 3, False),
+            ('conv5_3', 512, 1, 2, 3, True),
 
-            ('conv6_1', 28, 512, 1, 2, False, True),
-            ('conv6_2', 28, 512, 1, 2, False, True),
-            ('conv6_3', 28, 512, 1, 2, True, True),
+            ('conv6_1', 512, 1, 2, 3, False),
+            ('conv6_2', 512, 1, 2, 3, False),
+            ('conv6_3', 512, 1, 2, 3, True),
 
-            ('conv7_1', 28, 256, 1, 1, False, True),
-            ('conv7_2', 28, 256, 1, 1, False, True),
-            ('conv7_3', 28, 256, 1, 1, True, True),
+            ('conv7_1', 512, 1, 1, 3, False),
+            ('conv7_2', 512, 1, 1, 3, False),
+            ('conv7_3', 512, 1, 1, 3, True),
 
-            ('conv8_1', 56, 128, .5, 1, False, False),
-            ('conv8_2', 56, 128, 1, 1, False, True),
-            ('conv8_3', 56, 128, 1, 1, False, True),
+            ('conv8_1', 128, .5, 1, 4, False),
+            ('conv8_2', 128, 1, 1, 3, False),
+            ('conv8_3', 128, 1, 1, 3, False),
+
+            ('conv_out', 313, 1, 1, 1, False)
 
         )
 
-        for i, (label, X, C, S, D, BN, P) in enumerate(conv_layers):
+        for i, (label, C, S, D, K, BN) in enumerate(conv_layers):
             if S >= 1:
-
                 self.model.add(Conv2D(
                     filters=C,
-                    kernel_size=(1, 1),
+                    kernel_size=(K, K),
                     strides=S,
                     dilation_rate=D,
                     activation='relu',
-                    padding='same' if P else 'valid',
-                    name=label
+                    padding='same', # if P else 'valid',
+                    name=label,
+                    use_bias=True
                 ))
             else:
                 self.model.add(Conv2DTranspose(
                     filters=C,
-                    kernel_size=(1, 1),
+                    kernel_size=(K, K),
                     strides=int(1 / S),
                     dilation_rate=D,
                     activation='relu',
-                    padding='same' if P else 'valid',
-                    name=label
+                    padding='same', # if P else 'valid',
+                    name=label,
+                    use_bias=True
                 ))
 
             if BN:
@@ -122,3 +125,6 @@ class CNN:
         cv2.waitKey(0)
         # img = cv2.cvtColor(orig_img, cv2.COLOR_Lab2RGB)
         # plt.imshow(img / 255.0)
+
+m = CNN()
+
