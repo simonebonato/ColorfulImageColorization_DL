93d59488dbb45d5416b50d4120a3db8ad1def56d
simon <bonato@kth.se>
2022-05-05 09:54:55 +0200

    CustomGenerator

--- image_gen.py
+++ image_gen.py
@@ -1,4 +1,5 @@
 import cv2
+import keras.utils.data_utils
 import numpy as np
 from PIL import Image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -30,24 +31,6 @@ def plot_image_from_Lab(img, grayscale=False):
     plt.show()
 
 
-def custom_data_gen(path, img_size=256, batch_size=1):
-    x_datagen = ImageDataGenerator(preprocessing_function=get_L)
-    x_generator = x_datagen.flow_from_directory(directory=path,
-                                                target_size=(img_size, img_size),
-                                                batch_size=batch_size,
-                                                shuffle=False,
-                                                classes=None)
-
-    y_datagen = ImageDataGenerator(preprocessing_function=get_ab)
-    y_generator = y_datagen.flow_from_directory(directory=path,
-                                                target_size=(img_size, img_size),
-                                                batch_size=batch_size,
-                                                shuffle=False,
-                                                classes=None)
-    generator = zip(x_generator, y_generator)
-    return generator
-
-
 # def load_images_from_folder(self, folder):
 #     print('-- Reading images --')
 #     images = []
@@ -59,9 +42,80 @@ def custom_data_gen(path, img_size=256, batch_size=1):
 #             images.append(img)
 #     self.images = images
 
+def get_partitions(train_path, val_path):
+    from os import listdir
+    from os.path import isfile, join
+    train_imgs = [join(train_path, f) for f in listdir(train_path) if isfile(join(train_path, f))]
+    val_imgs = [join(val_path, f) for f in listdir(val_path) if isfile(join(val_path, f))]
+    return train_imgs, val_imgs
 
-g = custom_data_gen('data/train')
-for image, label in g:
-    plot_image_from_Lab(image[0][0])
-    # print(label)
+
+class DataGenerator(keras.utils.data_utils.Sequence):
+    def __init__(self, partition, batch_size=1, dim=(256, 256), n_channels=(1, 2), shuffle=True):
+        '''
+        partition: list with paths of images
+        dim: desired image dimension
+        channels: number of channels in X and Y
+        '''
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.partition = partition
+        self.n_channels = n_channels
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.partition))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, imgs_path_temp):
+        'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels[0]))
+        Y = np.empty((self.batch_size, *self.dim, self.n_channels[1]))
+
+        # Generate data
+        for i, ID in enumerate(imgs_path_temp):
+            img = cv2.imread(ID)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
+            img = cv2.resize(img, self.dim)
+
+            X[i, :, :] = img[:, :, 0]
+            Y[i, :, :] = img[:, :, 1:]
+        return X, Y
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.partition) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+
+        # Find list of IDs
+        partition_temp = [self.partition[k] for k in indexes]
+
+        # Generate data
+        X, Y = self.__data_generation(partition_temp)
+
+        return X, Y
+
+
+train_path = 'data/train'
+val_path = 'data/val'
+partition = {'train': (get_partitions(train_path, val_path))[0], 'val': (get_partitions(train_path, val_path))[1]}
+
+params = {'dim': (256, 256),
+          'batch_size': 10,
+          'n_channels': (1, 2),
+          'shuffle': True}
+
+training_generator = DataGenerator(partition['train'], **params)
+
+for i in training_generator:
+    print(i[1].shape)
     break
