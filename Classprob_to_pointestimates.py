98f87e146cd376d5d524617cd31c1a9de4cb7f81
simon <bonato@kth.se>
2022-05-11 11:43:46 +0200

    reconstruct func

--- Classprob_to_pointestimates.py
+++ Classprob_to_pointestimates.py
@@ -1,10 +1,11 @@
 import numpy as np
+import cv2
 
 
 def prob_to_point_est(Z, T=0.38):
     q_ab = np.load("pts_in_hull.npy")
 
-    # Z is a vector with dims [H, W, Q=313]
+    # Z is a vector with dims [64, 64, Q=313]
     # Each Q is a probability that the pixel has a specific gamut color
     new_p = np.zeros_like(Z)
     image_out = np.zeros(shape=(Z.shape[0], Z.shape[1], 2))
@@ -16,16 +17,32 @@ def prob_to_point_est(Z, T=0.38):
             ab = (new_p[h, w, :][:, np.newaxis] * q_ab).sum(axis=0)
             image_out[h, w, :] = ab
 
-    # Returns HxWx2 (Y)
+    # Returns 64x64x2 (Y)
     return image_out
 
+
+def reconstruct_image(X, y_pred):
+    """
+    X: CNN input - L channel [batch_size x H x W x 1]
+    y: CNN output - ab channels [batch_size x 64 x 64 x 2]
+    return: colored version of the image in RGB
+    """
+    batch_size = X.shape[0]
+    h, w = X.shape[1:3]
+    output_imgs = np.zeros(shape=(batch_size, h, w, 3))
+    for i in range(batch_size):
+        y = prob_to_point_est(y_pred[i])
+        ab_resized = cv2.resize(y, (h, w, 2), cv2.INTER_CUBIC)
+        output_imgs[i, :, :, 0] = X
+        output_imgs[i, :, :, 1:] = ab_resized
+    return output_imgs
 
 # q_ab = np.load("pts_in_hull.npy")
 # nb_q = q_ab.shape[0]
 #
-# probs = np.random.rand(256, 256, 313)
-# for i in range(probs.shape[0]):
-#     for y in range(probs.shape[1]):
-#         probs[i, y, :] /= np.sum(probs[i, y, :])
-#
-# image_out = prob_to_point_est(probs)
+probs = np.random.rand(256, 256, 313)
+for i in range(probs.shape[0]):
+    for y in range(probs.shape[1]):
+        probs[i, y, :] /= np.sum(probs[i, y, :])
+
+image_out = reconstruct_image(pro)
