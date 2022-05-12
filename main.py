a5e197cd7b49ef1b011274d8e905ae502cfaf707
vishalned <vishalned@gmail.com>
2022-05-12 12:39:11 +0200

    changed files for server run

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ def main():
     batch_size = 8
     train_path = 'data/train'
     val_path = 'data/val'
-
-    load_model = True
+    load_model = False
 
     partition = {'train': (get_partitions(train_path, val_path))[0],
                  'val': (get_partitions(train_path, val_path))[1]}
@@ -38,22 +37,22 @@ def main():
         model = tf.keras.models.load_model(r'Best_Model')
     else:
         model = CNN(input_shape, batch_size).get_model()
-        model_saver = ModelCheckpoint(filepath='Best_Model', monitor='val_loss',
+        model_saver = ModelCheckpoint(filepath='best_model', monitor='val_loss',
                                       save_best_only=True, mode='min')
         model.fit(
             x=training_generator,
-            epochs=20,
+            epochs=50,
             validation_data=validation_generator,
             callbacks=[model_saver],
             workers=1,
             use_multiprocessing=False)
 
 
-    test = training_generator.__getitem__(0)
-    test_X, test_Y = test[0], test[1]
-    y_pred = model.predict(test_X)
-    rec = reconstruct_image(test_X, y_pred)
-    plot_image_from_Lab(rec[0])
+    # test = training_generator.__getitem__(0)
+    # test_X, test_Y = test[0], test[1]
+    # y_pred = model.predict(test_X)
+    # rec = reconstruct_image(test_X, y_pred)
+    # plot_image_from_Lab(rec[0])
 
 
 if __name__ == '__main__':
