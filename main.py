5c3563ee4168e1de235515c69e27c2b5c73941a9
simon <bonato@kth.se>
2022-05-24 13:07:48 +0200

    .

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-import cv2
 import matplotlib.pyplot as plt
-import os
-from tqdm import tqdm
-import tensorflow as tf
 import numpy as np
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Conv2DTranspose, InputLayer, UpSampling2D
-from tensorflow.keras.models import Sequential
-from cnn_model import CNN
-from custom_adam import AdamWeightDecayOptimizer
-from tensorflow.keras.optimizers.schedules import ExponentialDecay
-from tensorflow.keras.callbacks import ModelCheckpoint
-from loss_function import *
-from image_generator import *
+
 from cnn_model import *
 from Classprob_to_pointestimates import *
 
@@ -19,10 +8,10 @@ from Classprob_to_pointestimates import *
 @tf.autograph.experimental.do_not_convert
 def main():
     input_shape = (256, 256)
-    batch_size = 8
+    batch_size = 10
     train_path = 'data/train'
     val_path = 'data/val'
-    saver_name = 'Smaller_Model'
+    saver_name = 'LessLayers'
 
     partition = {'train': (get_partitions(train_path, val_path))[0],
                  'val': (get_partitions(train_path, val_path))[1]}
@@ -48,21 +37,21 @@ def main():
         lr = ExponentialDecay(initial_learning_rate=3e-5, decay_steps=40623, decay_rate=0.8)
         adam = Adam(beta_1=0.9, beta_2=0.99, learning_rate=lr, clipvalue=5)
         model.compile(loss=L_cl2, optimizer=adam, run_eagerly=True)
-        initial_epoch = 7
+        initial_epoch = 9
     else:
         model = CNN(input_shape, batch_size).get_model()
         initial_epoch = 0
 
     model_saver = ModelCheckpoint(filepath=saver_name, monitor='val_loss',
                                   save_best_only=True, mode='min')
-    model.fit(
-        x=training_generator,
-        epochs=50,
-        validation_data=validation_generator,
-        callbacks=[model_saver],
-        workers=1,
-        use_multiprocessing=False,
-        initial_epoch=initial_epoch)
+    # model.fit(
+    #     x=training_generator,
+    #     epochs=50,
+    #     validation_data=validation_generator,
+    #     callbacks=[model_saver],
+    #     workers=1,
+    #     use_multiprocessing=False,
+    #     initial_epoch=initial_epoch)
 
     # Check quality of images
     test = training_generator.__getitem__(0)
@@ -73,13 +62,23 @@ def main():
     gt_images = reconstruct_gt_image(X=test_X, y_true=test_Y)
     images = reconstruct_image(X=test_X, y_pred=y_pred)
 
-    for i in range(images.shape[0]):
-        plt.subplot(1, 2, 1)
-        plt.title('Ground Truth Image')
+    positions = [1, 2]
+    gt_positions = []
+    for i in range(gt_images.shape[0]):
+        gt_pos = np.random.choice(positions)
+        fake_pos = 1 if gt_pos == 2 else 2
+        plt.subplot(1, 2, gt_pos)
+        # plt.title('Ground Truth Image')
         plot_image_from_Lab(img=gt_images[i], gt=True)
-        plt.subplot(1, 2, 2)
-        plt.title('Model Image')
-        plot_image_from_Lab(img=images[i])
+        plt.subplot(1, 2, fake_pos)
+        # plt.title('Model Image')
+        plot_image_from_Lab(img=images[i], savename=f'{saver_name}/imgs/pic_{i}_{gt_pos}.png')
+
+
+        gt_positions.append(gt_pos)
+
+
+    print(gt_positions)
 
 
 if __name__ == '__main__':
